ba1d6c35820503e31856ee789fc426cbdd033571
abdullahkhalil00 <abdullahkhalil4615@gmail.com>
2026-07-25 09:47:17 +0500

    feat: Implement FeaturedDestination and HotelCard components for room listings

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Hero from '../component/Hero'
+import FeaturedDestination from '../component/featureDestination'
 
 export const Home = () => {
   return (
     <>
 
         <Hero/>
+        <FeaturedDestination/>
     </>
   )
 }

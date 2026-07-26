5b79a548f743ce636d0a06e8a1d450c3acaf817d
abdullahkhalil00 <abdullahkhalil4615@gmail.com>
2026-07-26 14:04:10 +0500

    fix: ensure webhook verification is called correctly in clerkWebhook function

--- server/controllers/clerkWebHook.js
+++ server/controllers/clerkWebHook.js
@@ -16,6 +16,7 @@ const clerkWebhook = async (req, res) => {
             "svix-signature": req.headers["svix-signature"],
         }
         // Verify 
+                await whook.verify(req.rawBody || JSON.stringify(req.body), headers)
 
         // Getting Data from request body
         const { data, type } = req.body

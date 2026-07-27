fc822854fed8d6c9df18340150bf6b3f6eb111e3
abdullahkhalil00 <abdullahkhalil4615@gmail.com>
2026-07-27 15:16:17 +0500

    feat: implement protect middleware to verify user authentication via Clerk userId

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -14,7 +14,7 @@ async function protect(req, res, next) {
         });
     }
 
-    const user = await User.findById(userId); x``
+    const user = await User.findById(userId);
 
     console.log("user:", user);
 
